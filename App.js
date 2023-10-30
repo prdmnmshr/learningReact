93d52b4a55b9007ba5b8268ba2709bce1c8c03ba
prady2021 <pradyumna.mishra@kelltontech.com>
2023-10-30 21:24:38 +0530

    basic of react

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
-const parent = React.createElement(
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
 
-    "div",
-    { id: "parent" },
-    [
-        React.createElement("div", { id: "child1" }, [
-            React.createElement("h1", {}, "I am an h1 tag"),
-            React.createElement("h2", {}, "THis is h2 tag")
-        ])
-    ], React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "THis is h2 tag")
-    ])
-    // to over come this problem jsx introduced
+
+//JSX(transpiled before it reaches the JS) -PARCEL-Babel
+//JSX is not a HTML in javascript
+//Bable transpiled to react
+//When jsx is in single line you can write it without wrapping in () bracket otherwise we have to wrap it in ();
+//react element
+const Title = () => (
+  <h1 id="heading" className="head" tabIndex="1">
+    Page title - PPPP
+  </h1>
 );
-console.log();
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+  
+
+
+
+//Components
+// header is a Component
+// Button is a Component
+// input box is a Component
+//2types of component
+//1 - class based component - OLD
+//2-functional component -NEW
+//A function which returns a jsx is a functional component
+// Component composition means one component in another component
+const HeadingComponent = () => (
+  
+  <div id="container">
+    <Title />
+    <h1 className="heading">This is my functional component</h1>
+  </div>
+);
+
+const root  = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
+root.render(<HeadingComponent/>);
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # learningReact
+
+#what parcel does and below are the thing causes react faster
+-Dev build
+-Local server
+-HMR - hot module replacement
+-File watching alogorithm- written in c++
+-cahing - so faster build
+-minification for production build
+-Bulding
+-compress the files, remove white spaces
+-All these things can be done by the different type of packages
+-parcel manages all these things
+-constistent Hashing
+-Code splitting
+-Differential Bundling - it support apps for older browsers
+-Error handling
+-HTTPS
+-Tree shaking - remove unused code
+-Differnt dev and prod bundles
+
+#what is JSX
+JSX is a javascript element which help to create HTML like syntax in react
+//Javascript is code js engine can understand it
+
+//Before jsx goes to javascript engine parcel transpiled it to javscript
